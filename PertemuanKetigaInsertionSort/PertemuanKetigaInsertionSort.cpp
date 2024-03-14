506423bcb803ca985ca5b84c53347b55cef9a06b
anjanipodomi <anjanidihapsari@gmail.com>
2024-03-14 15:00:13 +0700

    Add project files.

---
+++ PertemuanKetigaInsertionSort/PertemuanKetigaInsertionSort.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int arr[20];
+int n;
+
+void input() {
+    while (true)
+    {
+        cout << "Masukkan Jumlah Data pada Array : ";
+        cin >> n;
+
+        if (n <= 20) {
+            break;
+        }
+        else
+        {
+            cout << "\nArray yang anda masukkan maksimal 20 Element.\n";
+        }
+    }
+    cout << endl;
+    cout << "====================" << endl;
+    cout << "Masukkan Element Array" << endl;
+    cout << "====================" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> arr[i]
+    }
+}
+
+int main()
+{
+    
+}
